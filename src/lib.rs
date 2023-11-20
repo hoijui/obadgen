590817dd2eead805dd00ac299908cfafca275b19
Robin Vobruba <hoijui.quaero@gmail.com>
2023-11-20 12:44:27 +0100

    Unuse trait-alias to be able to compile in stable rust [fix]

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-#![feature(trait_alias)]
-
 pub mod box_err;
 pub mod cert_gen;
 pub mod constants;
--- src/util/test.rs
+++ src/util/test.rs
@@ -2,15 +2,18 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-#[cfg(test)]
-pub trait Normal = Sized + Send + Sync + Unpin;
+// As of November 2023, this (trait alias)
+// would require `#![feature(trait_alias)]` in lib.rs,
+// and woudl cause compilation in rust stable to fail.
+// #[cfg(test)]
+// pub trait Normal = Sized + Send + Sync + Unpin;
 
 #[cfg(test)]
-pub fn is_normal<T: Normal>() {}
+pub fn is_normal<T: Sized + Send + Sync + Unpin>() {}
 
 /// This ensures that the supplied type is a comfortably convertible,
 /// wrapable and so on error type.
 /// This should be used to check on (at least)
 /// all public errors of a codebase.
 #[cfg(test)]
-pub fn is_good_error<T: Normal + std::error::Error>() {}
+pub fn is_good_error<T: Sized + Send + Sync + Unpin + std::error::Error>() {}
